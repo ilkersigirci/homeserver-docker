0a5371ec78b62e5d0579638bf01f1d129795ea0d
ilker.sigirci <sigirci.ilker@gmail.com>
2022-10-05 02:19:45 +0300

    ytdl-sub metadata refrest option added

--- configs/ytdl-sub/config/download_yt.sh
+++ configs/ytdl-sub/config/download_yt.sh
@@ -7,9 +7,13 @@
 # Default arg
 # ARG2=${2:-'Video'}
 
-# To test run with ytdl-sub --dry-run dl
+# NOTE: To test it, run with ytdl-sub --dry-run dl 
 
 # TODO: Run this without copying inside container
+
+
+# Download part of a youtube video ->> Command line version
+# yt-dlp https://www.youtube.com/watch?v=xMOHrl_is4Y --downloader ffmpeg --downloader-args "ffmpeg_i:-ss 00:00:01.00 -to 00:00:02.00"
 
 
 if [ -z "$1" ]
@@ -21,11 +25,16 @@ fi
 cd config
 
 if [ -z "$2" ]
-    then
-        echo "No folder name supplied, downloading to default 'Video' folder"
-        ytdl-sub dl --single-video-with-foldername --overrides.tv_show_name "Video" --youtube.video_url $1
-    else
-        # ytdl-sub dl --single-video "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        echo "Downloading to $2 folder"
-        ytdl-sub dl --single-video-with-foldername --overrides.tv_show_name "$2" --youtube.video_url $1
+then
+    echo "No folder name supplied, downloading to default 'Video' folder"
+    ytdl-sub dl --single-video $1
+elif [ $2 = "Metadata" ]
+then
+    echo "Downloading just metadata to Metadata folder"
+    ytdl-sub dl --single-video-metadata $1
+    # TODO: Remove .cache and .mp4 video file
+else
+    # ytdl-sub dl --single-video "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    echo "Downloading to $2 folder"
+    ytdl-sub --dry-run dl --single-video-with-foldername $1 --overrides.tv_show_name "$2"
 fi
