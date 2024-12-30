1512fb13c24f9c4d67c160af526021cd96a36b91
İlker SIĞIRCI <sigirci.ilker@gmail.com>
2024-12-30 15:06:01 +0300

    down script fix

--- scripts/desktop/down.sh
+++ scripts/desktop/down.sh
@@ -2,10 +2,14 @@
 
 STACKS_ROOT=~/docker/stacks
 
-stacks=("base" "notes" "media" "others" "desktop_apps" "maintenance" "monitoring" "photo" "ml")
+stacks=("base" "notes" "media" "others" "desktop_apps" "maintenance" "monitoring" "ml")
 
 for stack in "${stacks[@]}"
 do
     cd "$STACKS_ROOT/$stack"
     docker compose --env-file ~/docker/configs/.env down --volumes --remove-orphans
 done
+
+# Photo dir
+cd "$STACKS_ROOT/photo"
+docker compose down --volumes --remove-orphans
